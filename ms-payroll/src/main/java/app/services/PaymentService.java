c535c3262898c57a7d4c8fcf68337ba31de63a95
Wilker Chaves <chaveswill8@gmail.com>
2025-04-16 14:39:17 -0300

    Resttemplate impl

--- ms-payroll/src/main/java/app/services/PaymentService.java
+++ ms-payroll/src/main/java/app/services/PaymentService.java
@@ -1,15 +1,38 @@
 package app.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import app.models.Payment;
+import app.models.Worker;
 
 @Service
 public class PaymentService {
 
-	public Payment getPayment(long workerId, int days) {
+	private final RestTemplate restTemplate;
+	@Value("${ms-workers.host}")
+	private String workerHost;
 
-		return new Payment("Bob", 200.0, days);
+	public PaymentService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public Payment getPayment(long workerId, int days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+
+		Worker worker = this.restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		if (worker != null) {
+			return new Payment(worker.getName(), worker.getDailyIncome(), days);
+		}
+
+		return null;
+
 	}
 
 }
