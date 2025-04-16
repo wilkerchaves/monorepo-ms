3d86139bbc82dee25fd6f7ee316d50e1b2033412
Wilker Chaves <chaveswill8@gmail.com>
2025-04-16 18:04:57 -0300

    Eureka configuration

--- ms-payroll/src/main/java/app/MsPayrollApplication.java
+++ ms-payroll/src/main/java/app/MsPayrollApplication.java
@@ -2,10 +2,7 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@RibbonClient(name = "ms-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class MsPayrollApplication {
