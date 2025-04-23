51056eccc7be2fb93b30d139ffc833f19df47b74
Wilker Chaves <chaveswill8@gmail.com>
2025-04-23 17:02:45 -0300

    flyway configuration updated

--- ms-user/src/main/resources/db/migration/postgresql/V1745430822558__insert_into_role.sql
+++
@@ -1,2 +0,0 @@
-insert into tb_role (role_name) values ("ROLE_OPERATOR");
-insert into tb_role (role_name) values ("ROLE_ADMIN");
--- ms-user/src/main/resources/db/migration/postgresql/V1745430949045__insert_into_role_user.sql
+++
@@ -1,3 +0,0 @@
-insert into tb_user_role(user_id, role_id) values (1, 1);
-insert into tb_user_role(user_id, role_id) values (1, 2);
-insert into tb_user_role(user_id, role_id) values (2, 2);
